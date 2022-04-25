8567fcdea733b92747005a2183ebc763c4923ac1
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-04-25 19:51:37 +0200

    Homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <ul>
-                <li>Qui suis-je ?</li>
-                <li>Compétences</li>
-                <li>Portfolio</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="navbar">
+      <ul>
+        <li>Qui suis-je ?</li>
+        <li>Compétences</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Presentation = () => {
-    return (
-        <div className='presentation' id="presentation">
-            {/* <img src="src/assets/presentation_wave.png" alt="wave" /> */}
-            <p className='presentation__intro'>
-            {`<p> Bonjour, je suis {Nicolas}, développeur web junior, et je vous souhaite la bienvenue dans mon univers ! </p>`}
-            </p>
-        </div>
-    );
+  return (
+    <div className="presentation" id="presentation">
+      {/* <img src="src/assets/presentation_wave.png" alt="wave" /> */}
+      <p className="presentation__intro">
+        {`<p> Bonjour, je suis {Nicolas}, développeur web junior, et je vous souhaite la bienvenue dans mon univers ! </p>`}
+      </p>
+    </div>
+  );
 };
 
 export default Presentation;
