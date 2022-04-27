0543bdc9a644ab8ddc98f91055d7a297b04471c8
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-04-28 01:09:07 +0200

    leftbar

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -3,10 +3,21 @@ import React from "react";
 const Leftbar = () => {
   return (
     <div className="leftbar">
-      <div className="leftbar__logo" />
-      <div className="leftbar__logo" />
-      <div className="leftbar__logo" />
-      <div className="leftbar__logo" />
+      <div className="leftbar__logo">
+        <img src="src/assets/home2.png" alt="home" />
+      </div>
+      <div className="leftbar__logo">
+        <img src="src/assets/presentation.png" alt="pres" />
+      </div>
+      <div className="leftbar__logo">
+        <img src="src/assets/tools.png" alt="tools" />
+      </div>
+      <div className="leftbar__logo">
+        <img src="src/assets/projects.png" alt="projects" />
+      </div>
+      <div className="leftbar__logo">
+        <img src="src/assets/contact.png" alt="contact" />
+      </div>
     </div>
   );
 };
