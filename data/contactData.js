78c887bdd2308aad843b2b51d70eb11771e06968
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-05-18 21:07:20 +0200

    contact 2

---
+++ data/contactData.js
@@ -0,0 +1,19 @@
+const contactLogo = [
+  {
+    id: "linkedin",
+    img: "./assets/linkedin.svg",
+    url: "https://www.linkedin.com/in/nicolas-vagnoux/",
+  },
+  {
+    id: "github",
+    img: "./assets/github2.svg",
+    url: "https://github.com/NicolasVagnoux",
+  },
+  {
+    id: "gmail",
+    img: "./assets/gmail.svg",
+    url: "mailto:nicolas.vagnoux@gmail.com",
+  },
+];
+
+export default contactLogo;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import logos from "../../data/contactData";
+import ContactLogo from "./ContactLogo";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [message, setMessage] = useState("");
+  console.log(name, mail, message);
+
   return (
     <div className="contact" id="contact">
       <h1 className="contact__title">{"< Contact />"}</h1>
       <form className="contact__form" autoComplete="off">
         <p>
-          Si vous souhaitez me contacter, vous pouvez remplir le formulaire
+          Vous souhaitez me contacter ? Vous pouvez remplir le formulaire
           suivant :
         </p>
         <div className="contact__form__name">
@@ -16,18 +23,30 @@ const Contact = () => {
             type="text"
             required
             autoComplete="false"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
           />
-          <label htmlFor="name">Quel est votre nom ?</label>
+          <label htmlFor="name" className={name ? "inputHasValue" : ""}>
+            Quel est votre nom ?
+          </label>
         </div>
         <div className="contact__form__mail">
           <input
             className="contact__form__mail__input"
             id="mail"
-            type="text"
+            type="email"
             required
             autoComplete="false"
+            value={mail}
+            onChange={(e) => {
+              setMail(e.target.value);
+            }}
           />
-          <label htmlFor="mail">Quelle est votre adresse email ?</label>
+          <label htmlFor="mail" className={mail ? "inputHasValue" : ""}>
+            Quelle est votre adresse email ?
+          </label>
         </div>
         <div className="contact__form__message">
           <textarea
@@ -35,11 +54,36 @@ const Contact = () => {
             id="message"
             rows="5"
             required
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
           />
-          <label htmlFor="message">Quel est votre message ?</label>
+          <label
+            htmlFor="message"
+            className={message ? "textareaHasValue" : ""}
+          >
+            Quel est votre message ?
+          </label>
         </div>
         <input className="contact__form__submit" type="submit" />
       </form>
+      <div className="contact__footer">
+        <div className="contact__footer__logos">
+          <p>Où me retrouver :</p>
+          <ul className="contact__footer__logos__list">
+            {logos.map((logo) => (
+              <ContactLogo {...logo} />
+            ))}
+          </ul>
+        </div>
+        <div className="contact__footer__cv">
+          <p>Télécharger mon CV :</p>
+          <a href="https://www.linkedin.com/in/nicolas-vagnoux/">
+            <img src="./assets/cv.png" alt="cv" width="90px" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
