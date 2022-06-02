12a4e04b90f65fdbbdc2119f212588ddb50a62a4
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-06-02 18:52:57 +0200

    CSS Updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Fade from "react-reveal/Fade";
 import logos from "../../data/contactData";
 import ContactLogo from "./ContactLogo";
 import cv from "../../public/assets/files/NicolasVagnouxCV_WebDev.pdf";
@@ -25,90 +26,96 @@ const Contact = () => {
 
   return (
     <div className="contact" id="contact">
-      <h1 className="contact__title">Contact</h1>
-      <form
-        className="contact__form"
-        autoComplete="off"
-        onSubmit={(e) => handleSubmitMail(e)}
-      >
-        <p>
-          Vous souhaitez me contacter ? Vous pouvez remplir le formulaire
-          suivant :
+      <Fade bottom>
+        <h1 className="contact__title">
+          <div />
+          Contact
+          <div />
+        </h1>
+        <form
+          className="contact__form"
+          autoComplete="off"
+          onSubmit={(e) => handleSubmitMail(e)}
+        >
+          <p>
+            Vous souhaitez me contacter ? Vous pouvez remplir le formulaire
+            suivant :
+          </p>
+          <div className="contact__form__name">
+            <input
+              className="contact__form__name__input"
+              id="name"
+              type="text"
+              required
+              autoComplete="false"
+              value={userName}
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
+            <label htmlFor="name" className={userName ? "inputHasValue" : ""}>
+              Quel est votre nom ?
+            </label>
+          </div>
+          <div className="contact__form__mail">
+            <input
+              className="contact__form__mail__input"
+              id="mail"
+              type="email"
+              required
+              autoComplete="false"
+              value={userMail}
+              onChange={(e) => {
+                setUserMail(e.target.value);
+              }}
+            />
+            <label htmlFor="mail" className={userMail ? "inputHasValue" : ""}>
+              Quelle est votre adresse email ?
+            </label>
+          </div>
+          <div className="contact__form__message">
+            <textarea
+              className="contact__form__message__input"
+              id="message"
+              rows="5"
+              required
+              value={userMessage}
+              onChange={(e) => {
+                setUserMessage(e.target.value);
+              }}
+            />
+            <label
+              htmlFor="message"
+              className={userMessage ? "textareaHasValue" : ""}
+            >
+              Quel est votre message ?
+            </label>
+          </div>
+          <button className="contact__form__submit" type="submit">
+            {status}
+          </button>
+        </form>
+        <p className="contact__prefooter">
+          ﹀ Ou si vous préférez me contacter autrement ﹀
         </p>
-        <div className="contact__form__name">
-          <input
-            className="contact__form__name__input"
-            id="name"
-            type="text"
-            required
-            autoComplete="false"
-            value={userName}
-            onChange={(e) => {
-              setUserName(e.target.value);
-            }}
-          />
-          <label htmlFor="name" className={userName ? "inputHasValue" : ""}>
-            Quel est votre nom ?
-          </label>
+        <div className="contact__footer">
+          <div className="contact__footer__logos">
+            <p>Où me retrouver :</p>
+            <ul className="contact__footer__logos__list">
+              {logos.map((logo) => (
+                <ContactLogo {...logo} />
+              ))}
+            </ul>
+          </div>
+          <div className="contact__footer__inter" />
+          <div className="contact__footer__cv">
+            <p>Télécharger mon CV :</p>
+            <a href={cv} target="blank">
+              <img src="./assets/cv.png" alt="cv" width="75px" />
+            </a>
+          </div>
         </div>
-        <div className="contact__form__mail">
-          <input
-            className="contact__form__mail__input"
-            id="mail"
-            type="email"
-            required
-            autoComplete="false"
-            value={userMail}
-            onChange={(e) => {
-              setUserMail(e.target.value);
-            }}
-          />
-          <label htmlFor="mail" className={userMail ? "inputHasValue" : ""}>
-            Quelle est votre adresse email ?
-          </label>
-        </div>
-        <div className="contact__form__message">
-          <textarea
-            className="contact__form__message__input"
-            id="message"
-            rows="5"
-            required
-            value={userMessage}
-            onChange={(e) => {
-              setUserMessage(e.target.value);
-            }}
-          />
-          <label
-            htmlFor="message"
-            className={userMessage ? "textareaHasValue" : ""}
-          >
-            Quel est votre message ?
-          </label>
-        </div>
-        <button className="contact__form__submit" type="submit">
-          {status}
-        </button>
-      </form>
-      <p className="contact__prefooter">
-        ﹀ Ou si vous préférez me contacter autrement ﹀
-      </p>
-      <div className="contact__footer">
-        <div className="contact__footer__logos">
-          <p>Où me retrouver :</p>
-          <ul className="contact__footer__logos__list">
-            {logos.map((logo) => (
-              <ContactLogo {...logo} />
-            ))}
-          </ul>
-        </div>
-        <div className="contact__footer__inter" />
-        <div className="contact__footer__cv">
-          <p>Télécharger mon CV :</p>
-          <a href={cv} target="blank">
-            <img src="./assets/cv.png" alt="cv" width="75px" />
-          </a>
-        </div>
-      </div>
+      </Fade>
     </div>
   );
 };
--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,37 +1,40 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import Fade from "react-reveal/Fade";
 
 const Presentation = () => {
   return (
     <div className="presentation" id="presentation">
-      <div className="presentation__core">
-        <div className="presentation__core__picture">
-          <p>Hello there !</p>
-          <img src="./assets/nv.jpg" alt="nv_pic" />
+      <Fade bottom>
+        <div className="presentation__core">
+          <div className="presentation__core__picture">
+            <p>Hello there !</p>
+            <img src="./assets/nv.jpg" alt="nv_pic" />
+          </div>
+          <p className="presentation__core__text">
+            Je m'appelle <span>Nicolas Vagnoux</span>, j'ai 27 ans, et je suis{" "}
+            <span>développeur Web Junior</span>. <br />
+            <br />
+            Après quelques années passées dans le monde de la logistique, j'ai
+            récemment décidé de me lancer dans une reconversion dans l'univers
+            de la tech, à travers une formation intensive de{" "}
+            <span>Développeur Web FullStack</span>.<br />
+            <br />
+            Désormais jeune développeur dans un domaine qui me passionne
+            réellement, je souhaite avant tout acquérir de l'
+            <span>expérience</span>, développer mes <span>compétences</span>, et
+            mettre mon savoir-faire au service de{" "}
+            <span>projets inspirants et innovants</span> !<br />
+            <br />
+            Bienvenue dans mon univers !
+          </p>
+          <img
+            className="presentation__core__text__rocket"
+            src="./assets/space.png"
+            alt="spaceship"
+          />
         </div>
-        <p className="presentation__core__text">
-          Je m'appelle <span>Nicolas Vagnoux</span>, j'ai 27 ans, et je suis{" "}
-          <span>développeur Web Junior</span>. <br />
-          <br />
-          Après quelques années passées dans le monde de la logistique, j'ai
-          récemment décidé de me lancer dans une reconversion dans l'univers de
-          la tech, à travers une formation intensive de{" "}
-          <span>Développeur Web FullStack</span>.<br />
-          <br />
-          Désormais jeune développeur dans un domaine qui me passionne
-          réellement, je souhaite avant tout acquérir de l'
-          <span>expérience</span>, développer mes <span>compétences</span>, et
-          mettre mon savoir-faire au service de{" "}
-          <span>projets inspirants et innovants</span> !<br />
-          <br />
-          Bienvenue dans mon univers !
-        </p>
-        <img
-          className="presentation__core__text__rocket"
-          src="./assets/space.png"
-          alt="spaceship"
-        />
-      </div>
+      </Fade>
     </div>
   );
 };
