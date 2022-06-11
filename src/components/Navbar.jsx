7a7c37569f4b89a6725b19142d660a82545698f3
Nicolas Vagnoux <nicolas.vagnoux@gmail.com>
2022-06-11 16:56:54 +0200

    added burger menu for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
-import { navbar } from "../../data/navData.js";
+import Icon from "@mdi/react";
+import { mdiMenuOpen, mdiClose } from "@mdi/js";
+import { navbar, leftbar } from "../../data/navData.js";
 
 const Navbar = () => {
+  const [isNavMobileOpened, setIsNavMobileOpened] = useState(false);
+
   return (
-    <div className="navbar">
-      <ul className="navbar__list">
-        {navbar.map((navItem) => (
-          <Link
-            activeClass="active"
-            to={navItem.link}
-            smooth
-            duration={600}
-            className="navbar__list__item"
+    <>
+      <div className="navbar">
+        <ul className="navbar__list">
+          {navbar.map((navItem) => (
+            <Link
+              activeClass="active"
+              to={navItem.link}
+              smooth
+              duration={600}
+              className="navbar__list__item"
+            >
+              <li>{navItem.name}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div
+        className={`navbarMobile ${
+          isNavMobileOpened && "navbarMobile--opened"
+        }`}
+      >
+        {!isNavMobileOpened ? (
+          <button
+            type="button"
+            onClick={() => {
+              setIsNavMobileOpened(true);
+            }}
           >
-            <li>{navItem.name}</li>
-          </Link>
-        ))}
-      </ul>
-    </div>
+            <Icon path={mdiMenuOpen} size={3} color="aliceblue" />
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={() => {
+              setIsNavMobileOpened(false);
+            }}
+          >
+            <Icon path={mdiClose} size={3} color="aliceblue" />
+          </button>
+        )}
+        <ul className="navbarMobile__list">
+          {leftbar.map((navItem) => (
+            <Link
+              activeClass="active"
+              to={navItem.link}
+              smooth
+              duration={600}
+              className="navbarMobile__list__item"
+            >
+              <img src={navItem.img} alt={navItem.link} />
+              <p>{navItem.name}</p>
+            </Link>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
